bf894aeb267e8a09f40fbaf28776aceb8cacf66a
Muhammad Rizki Malik Aziz <rizkimalik221001@gmail.com>
2024-04-15 21:35:27 +0700

    feat: change cursor to fps dot

--- src/views/components/Cursor.jsx
+++ src/views/components/Cursor.jsx
@@ -1,22 +1,26 @@
+import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import useMousePosition from '../../assets/js/cursorPosition';
 
 function Cursor() {
   const { x, y } = useMousePosition();
+  const [clicked, setClicked] = useState(false);
 
   return (
     <AnimatePresence>
       <motion.div
-        key="cursor" 
-        className={"cursor"}
-        initial={{ opacity: 0 }} 
-        animate={{ opacity: 1 }}
+        className="cursor"
         style={{ transform: `translate(${x}px, ${y}px)` }}
+        onClick={() => setClicked(true)}
       >
-        <div className="cursor_top"></div>
-        <div className="cursor_bottom"></div>
-        <div className="cursor_left"></div>
-        <div className="cursor_right"></div>
+        <div className={`cursor_corner cursor_top_left ${clicked ? 'clicked' : ''}`}></div>
+        <div className={`cursor_corner cursor_top_right ${clicked ? 'clicked' : ''}`}></div>
+        <div className={`cursor_corner cursor_top_left2 ${clicked ? 'clicked' : ''}`}></div>
+        <div className={`cursor_corner cursor_top_right2 ${clicked ? 'clicked' : ''}`}></div>
+        <div className={`cursor_corner cursor_bottom_left ${clicked ? 'clicked' : ''}`}></div>
+        <div className={`cursor_corner cursor_bottom_right ${clicked ? 'clicked' : ''}`}></div>
+        <div className={`cursor_corner cursor_bottom_left2 ${clicked ? 'clicked' : ''}`}></div>
+        <div className={`cursor_corner cursor_bottom_right2 ${clicked ? 'clicked' : ''}`}></div>
       </motion.div>
     </AnimatePresence>
   );
